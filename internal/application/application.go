d6bb22b391502afbe492e034f0d2067abe09b6d8
Ønelife <amdemidof2009@gmail.com>
2024-12-22 17:55:11 +0300

    applicaton.go upd

--- internal/application/application.go
+++ internal/application/application.go
@@ -36,26 +36,19 @@ func New() *Application {
 	}
 }
 
-// Функция запуска приложения
-// тут будем чиать введенную строку и после нажатия ENTER писать результат работы программы на экране
-// если пользователь ввел exit - то останаваливаем приложение
 func (a *Application) Run() error {
 	for {
-		// читаем выражение для вычисления из командной строки
 		log.Println("input expression")
 		reader := bufio.NewReader(os.Stdin)
 		text, err := reader.ReadString('\n')
 		if err != nil {
 			log.Println("failed to read expression from console")
 		}
-		// убираем пробелы, чтобы оставить только вычислемое выражение
 		text = strings.TrimSpace(text)
-		// выходим, если ввели команду "exit"
 		if text == "exit" {
 			log.Println("aplication was successfully closed")
 			return nil
 		}
-		//вычисляем выражение
 		result, err := calculation.Calc(text)
 		if err != nil {
 			log.Println(text, " calculation failed wit error: ", err)
@@ -92,6 +85,6 @@ func CalcHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (a *Application) RunServer() error {
-	http.HandleFunc("/", CalcHandler)
+	http.HandleFunc("/api/v1/calculate", CalcHandler)
 	return http.ListenAndServe(":"+a.config.Addr, nil)
 }
