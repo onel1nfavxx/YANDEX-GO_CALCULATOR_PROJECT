bd02660608a3ae3fc435ca273047547802fb03d8
Ønelife <amdemidof2009@gmail.com>
2025-01-08 14:33:22 +0300

    Update calculation_test.go

--- pkg/calculation/calculation_test.go
+++ pkg/calculation/calculation_test.go
@@ -2,37 +2,77 @@ package calculation
 
 import (
 	"testing"
-
-	"github.com/onel1nfavxx/YANDEX-GO_CALCULATOR_PROJECT/custom_errors"
 )
 
 func TestCalc(t *testing.T) {
-	tests := []struct {
-		expression string
-		expected   float64
-		expectErr  error
+	testCasesSuccess := []struct {
+		name           string
+		expression     string
+		expectedResult float64
 	}{
-		{"3 + 5", 8, nil},
-		{"10 - 2", 8, nil},
-		{"4 * 2", 8, nil},
-		{"16 / 2", 8, nil},
-		{"3 + 5 * 2", 13, nil},
-		{"(1 + 2) * 3", 9, nil},
-		{"(1 + 2) / 3", 1, nil},
-		{"2 * (3 + 5)", 16, nil},
-		{"2 * (3 + 5) - 4", 12, nil},
-		{"2 * (3 + 5) / 4", 4, nil},
-		{"2 * (3 + 5) / 0", 0, custom_errors.ErrDivisionByZero},
-		{"3 +", 0, custom_errors.ErrInvalidExpression},
-		{"* 3 + 5", 0, custom_errors.ErrInvalidExpression},
-		{"3 + a", 0, custom_errors.ErrInvalidExpression},
-		{"", 0, custom_errors.ErrInvalidExpression},
+		{
+			name:           "simple",
+			expression:     "1+1",
+			expectedResult: 2,
+		},
+		{
+			name:           "priority",
+			expression:     "(2+2)*2",
+			expectedResult: 8,
+		},
+		{
+			name:           "priority",
+			expression:     "2+2*2",
+			expectedResult: 6,
+		},
+		{
+			name:           "/",
+			expression:     "1/2",
+			expectedResult: 0.5,
+		},
 	}
 
-	for _, test := range tests {
-		result, err := Calc(test.expression)
-		if result != test.expected || (err != nil && err.Error() != test.expectErr.Error()) {
-			t.Errorf("Calc(%q) = %v, %v; want %v, %v", test.expression, result, err, test.expected, test.expectErr)
-		}
+	for _, testCase := range testCasesSuccess {
+		t.Run(testCase.name, func(t *testing.T) {
+			val, err := Calc(testCase.expression)
+			if err != nil {
+				t.Fatalf("successful case %s returns error", testCase.expression)
+			}
+			if val != testCase.expectedResult {
+				t.Fatalf("%f should be equal %f", val, testCase.expectedResult)
+			}
+		})
+	}
+
+	testCasesFail := []struct {
+		name        string
+		expression  string
+		expectedErr error
+	}{
+		{
+			name:       "simple",
+			expression: "1+1*",
+		},
+		{
+			name:       "priority",
+			expression: "2+2**2",
+		},
+		{
+			name:       "priority",
+			expression: "((2+2-*(2",
+		},
+		{
+			name:       "/",
+			expression: "",
+		},
+	}
+
+	for _, testCase := range testCasesFail {
+		t.Run(testCase.name, func(t *testing.T) {
+			val, err := Calc(testCase.expression)
+			if err == nil {
+				t.Fatalf("expression %s is invalid but result  %f was obtained", testCase.expression, val)
+			}
+		})
 	}
 }
