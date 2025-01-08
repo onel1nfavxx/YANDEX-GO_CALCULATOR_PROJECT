5df0a05373b2056f63f53b82262a396526771249
Ønelife <amdemidof2009@gmail.com>
2025-01-08 18:16:07 +0300

    Update errors.go

--- custom_errors/errors.go
+++ custom_errors/errors.go
@@ -3,12 +3,6 @@ package custom_errors
 import "errors"
 
 var (
-	ErrEmptyString          = errors.New("input string is empty")
-	ErrEndWithOperator      = errors.New("expression can't end with operation")
-	ErrStartWithOperator    = errors.New("expression can't start with operation")
-	ErrDivisionByZero       = errors.New("division by zero is not allowed")
-	ErrTwoConsecOperation   = errors.New("expression can't hold two consecutive operation")
-	ErrNoClosingParenthesis = errors.New("missing )")
-	ErrInvInputs            = errors.New("invalid input")
-	ErrInvalidExpression    = errors.New("invalid expression")
+	ErrDivisionByZero = errors.New("division by zero is not allowed")
+	ErrInvInputs      = errors.New("invalid input")
 )
